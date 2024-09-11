3192727284395f251f518311172713798ded3439
Vedant <vedant@Vedants-MacBook-Air.local>
2024-09-11 14:21:41 -0400

    first

---
+++ Lab 0/src/Model/HelloWorld.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author vedant
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello, World!");
+    }
+}
