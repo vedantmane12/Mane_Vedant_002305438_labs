aea9af4a4d5e5eda8f09dd886eaec9d89ce66688
Vedant <vedant@Vedants-MacBook-Air.local>
2024-09-22 18:10:36 -0400

    Lab 02 Complete

---
+++ Lab 2/src/model/VitalSigns.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author vedant
+ */
+public class VitalSigns {
+    String date;
+    double bloodPressure;
+    double temperature;
+    int pulse;
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public double getBloodPressure() {
+        return bloodPressure;
+    }
+
+    public void setBloodPressure(double bloodPressure) {
+        this.bloodPressure = bloodPressure;
+    }
+
+    public double getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(double temperature) {
+        this.temperature = temperature;
+    }
+
+    public int getPulse() {
+        return pulse;
+    }
+
+    public void setPulse(int pulse) {
+        this.pulse = pulse;
+    }
+
+    @Override
+    public String toString(){
+        return getDate();
+    }
+
+}
