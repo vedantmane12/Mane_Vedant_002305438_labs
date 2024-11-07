bda98cfdf9566059119c1dbec3dd12772856a8a6
Vedant <vedant@Vedants-MacBook-Air.local>
2024-11-07 18:25:38 -0500

    Model elements added to Lab 6

--- Lab 6/src/model/Product.java
+++ Lab 6/src/model/Product.java
@@ -12,8 +12,9 @@ package model;
 public class Product {
 
     private String prodName;
-    private int price;
+    private double price;
     private int modelNumber;
+    private int avail;
 
     private static int count = 0;
 
@@ -35,14 +36,22 @@ public class Product {
         this.prodName = prodName;
     }
 
-    public int getPrice() {
+    public double getPrice() {
         return price;
     }
 
-    public void setPrice(int price) {
+    public void setPrice(double price) {
         this.price = price;
     }
 
+    public int getAvail() {
+        return avail;
+    }
+
+    public void setAvail(int avail) {
+        this.avail = avail;
+    }
+    
     public int getModelNumber() {
         return modelNumber;
     }
